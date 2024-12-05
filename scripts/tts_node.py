93ea524fa61399aaa2276400062ecf28a9178d7b
Ausaurus <92192784+Ausaurus@users.noreply.github.com>
2024-12-05 23:49:11 +0800

    Update tts_node.py

--- scripts/tts_node.py
+++ scripts/tts_node.py
@@ -12,37 +12,26 @@ def callback(data):
     soundhandle.say(text)
 
 def put_the_phone_down(data):
-    # Audio configuration
     CHUNK = 1024
-    # Initialize PyAudio
     audio = pyaudio.PyAudio()
     f = wave.open(r"/home/manfred/catkin_ws/src/edu_robot_pkg/audio/PUT THE PHONE DOWN.wav","rb")
     stream = audio.open(format = audio.get_format_from_width(f.getsampwidth()),  
                 channels = f.getnchannels(),  
                 rate = f.getframerate(),  
                 output = True)  
-    #read data  
     data = f.readframes(CHUNK)  
-    
-    #play stream  
     while data:  
         stream.write(data)  
         data = f.readframes(CHUNK)  
-    
-    #stop stream  
     stream.stop_stream()  
     stream.close()  
-    
-    #close PyAudio  
     audio.terminate() 
 
 
 if __name__ == '__main__':
     rospy.init_node('tts_node')
-    
-    # Initialize SoundClient with a slight delay for safety
     soundhandle = SoundClient()
-    time.sleep(1)  # Ensure sound client initializes correctly
+    time.sleep(1)
     rospy.loginfo("SoundClient initialized successfully.")
     rospy.Subscriber('tts_input', String, callback)
     rospy.Subscriber('Phone_detection', String, put_the_phone_down)
